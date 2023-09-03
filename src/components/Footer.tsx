c658c9e4ce1082ee2a7f4694e0c67c2db53bad2d
perlpark <hello@jinjoo.me>
2023-09-03 16:04:01 +0900

    Footer에 Button 컴포넌트 적용 및 스타일링 수정

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,15 @@
+import Button from "./Button";
+
 function Footer() {
   return (
-    <footer className="border-t">
-      <div className="max-w-md flex justify-between mx-auto">
-        <button type="button">load</button>
-        <button type="button">초기화</button>
+    <footer className="border-t py-4 fixed w-full bottom-0 bg-white">
+      <div className="max-w-3xl flex justify-between gap-4 mx-auto">
+        <Button type="primary" size="full">
+          load
+        </Button>
+        <Button type="secondary" size="full">
+          초기화
+        </Button>
       </div>
     </footer>
   );
