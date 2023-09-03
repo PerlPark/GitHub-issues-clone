5f05dd43df3555d9883ff880d2cfec29ae9fc8de
perlpark <hello@jinjoo.me>
2023-09-03 17:31:55 +0900

    상세 화면 이동 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
+import Detail from "./pages/Detail";
 import Issues from "./pages/Issues";
 
 function App() {
-  return (
-    <main>
-      <Issues />
-    </main>
-  );
+  const path = window.location.pathname.split("/");
+  const isDetail = path[1] === "detail" && path[2];
+
+  return <main>{isDetail ? <Detail /> : <Issues />}</main>;
 }
 
 export default App;
---
+++ src/pages/Detail.tsx
@@ -0,0 +1,5 @@
+function Detail() {
+  return <div>상세</div>;
+}
+
+export default Detail;
