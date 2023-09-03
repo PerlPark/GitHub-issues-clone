8da95dcb598c1a1d729ff1c1e68fd49b2d259b7e
perlpark <hello@jinjoo.me>
2023-09-03 15:03:15 +0900

    useGetIssues API 호출 훅 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+import { useEffect } from "react";
 import "./App.css";
+import useGetIssues from "./hooks/useGetIssues";
 
 function App() {
+  const { data, isError } = useGetIssues();
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
+
+  if (isError) {
+    return <div className="App">API 호출 오류가 발생했습니다.</div>;
+  }
+
   return <div className="App">TEST</div>;
 }
 
---
+++ src/hooks/useGetIssues.ts
@@ -0,0 +1,26 @@
+import { Octokit } from "octokit";
+import { useEffect, useState } from "react";
+
+const useGetIssues = () => {
+  const [data, setData] = useState([]);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    const octokit = new Octokit({
+      auth: process.env.REACT_APP_GITHUB_TOKEN,
+    });
+
+    octokit
+      .request("GET /repos/angular/angular-cli/issues", {
+        headers: {
+          "X-GitHub-Api-Version": "2022-11-28",
+        },
+      })
+      .then((res) => setData(res.data))
+      .catch(() => setIsError(true));
+  }, []);
+
+  return { data, isError };
+};
+
+export default useGetIssues;
