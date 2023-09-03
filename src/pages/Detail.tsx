4c8b819862650c959e0fbca6326e2fcbcda33cd3
perlpark <hello@jinjoo.me>
2023-09-03 18:14:32 +0900

    Detail 데이터 호출

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -1,4 +1,13 @@
+import { useAppSelector } from "../app/hooks";
+import useGetIssueDetail from "../hooks/useGetIssueDetail";
+
 function Detail() {
+  const number = useAppSelector((state) => state.issueDetail.number);
+
+  const { data } = useGetIssueDetail({ number });
+
+  console.log(data);
+
   return <div>상세</div>;
 }
 
